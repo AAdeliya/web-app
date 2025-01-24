3f81c47480d8cb2eb42b834a5fd4a488a2ce3b36
firstPush <adel.sdet2@gmail.com>
2025-01-23 23:01:44 -0600

    different approaches for event handler binding

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -7,13 +7,23 @@ import React, { Component } from 'react'
       this.state = {
         message : 'Hello'
       }
+      //this.clickHandler = this.clickHandler.bind(this)
     }
-    clickHandler() {
-        // this.setState({
-        //     message : 'Goodbye!'
+    // clickHandler() {
+    //     this.setState({
+    //         message : 'Goodbye!'
 
-        // })
+    //     })
 
+    // }
+
+
+
+    //Forth approach  - class property as a clas
+    clickHandler = () => {
+      this.setState({
+        message : 'Goodbuy'
+      })
     }
 
 
@@ -21,7 +31,17 @@ import React, { Component } from 'react'
     return (
       <div>
         <div>{this.state.message}</div>
-        <button onClcik = {this.clickHandler}>Click</button>
+
+        //first aproach for event handler  - binding in render (low performance)
+        {/*<button onClcik = {this.clickHandler.bind(this)}>Click</button>*/}
+
+
+        //second approach - arrow function in render (easiest way to past parameters)
+        {/*<button onClick={() => this.clickHandler()}>Click</button> */}
+
+        //third approach - binding in the class constructor, this is better the binding in the render method - suggestions from REACT
+
+        <button onClick={this.clickHandler}>Click </button>
         </div>
     )
   }
