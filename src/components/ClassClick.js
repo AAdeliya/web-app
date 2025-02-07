3f27a1acfe432826887eecbf0d69e36937a06fb8
firstPush <adel.sdet2@gmail.com>
2025-02-06 18:50:17 -0600

    ClassClick.js

--- src/components/ClassClick.js
+++ src/components/ClassClick.js
@@ -7,7 +7,7 @@ class ClassClick extends Component {
     render() {
         return (
     <div>
-        <button onClick={this.clickHandler}>Click me</button>
+        <button onClick={this.clickHandler}> Click me </button>
         </div>
   )
     }
