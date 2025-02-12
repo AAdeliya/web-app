ed34087e7f509008f0e0f8ab7b7267be1f6f1f79
firstPush <adel.sdet2@gmail.com>
2025-02-12 01:17:12 -0600

    List Rendering

---
+++ src/components/Person.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Person() {
+  return (
+    // <h2>
+    //     I am {personalbar.name}. I am {personalbar.age} years old
+    // </h2>
+    <div>Person</div>
+  )
+}
+
+export default Person
