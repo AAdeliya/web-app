044ea1b05cfafa294550846472219c4b46268e44
firstPush <adel.sdet2@gmail.com>
2025-02-15 14:16:58 -0600

    NameList();

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function NameList() {
-    const names = ['Bruce', 'Clark', 'Diane']
+    const names = ['Bruce', 'Clark', 'Diane', 'Adel']
     const nameList =  names.map((name, index) => <h2 key = {index}>{index} {name}</h2>)
   return (
     <div>
