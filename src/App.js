7317ca0fd1245dfece9436c7a37dfdcf61e8bf4c
firstPush <adel.sdet2@gmail.com>
2025-02-16 01:13:00 -0600

    Styling React Components

--- src/App.js
+++ src/App.js
@@ -11,11 +11,17 @@ import EventBind from './components/EventBind';
 import ParentComponent from './components/ParentComponent';
 import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
+import './appStyles.css'
+import styles from './appStyles.module.css'
 
 class App extends Component {
   render () {
     return (
     <div className="App">
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      <Incline></Incline>
+      <StyleSheet primary={true}></StyleSheet>
       <NameList></NameList>
       <UserGreeting></UserGreeting>
       <ParentComponent></ParentComponent>
